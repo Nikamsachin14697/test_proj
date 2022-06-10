3fc9f641faec0790dc9a67b132bf0535d2a34376
github_username <email_address>
2022-06-10 12:54:16 +0530

    added prime number code

---
+++ prime.py
@@ -0,0 +1,43 @@
+
+def prime_composite(num_list):
+  prime =[]
+  composite =[]
+  output = []
+  for i in num_list:
+    
+    flag=0
+    for j in range(2,i):
+      
+      if(i%j==0):
+        flag =1
+        composite.append(i)
+        break
+        
+    if(flag==0):
+        prime.append(i)
+
+
+  if flag==0:
+      print(0)
+  else:
+      print(1)
+  output.append([prime] + [composite])
+  return output
+
+
+
+
+
+if __name__=="__main__":
+  
+  num_list = []
+  count = int(input())
+  
+  for i in range(count):
+    num_list.append(int(input()))
+    
+  result = prime_composite(num_list)
+  
+  if result!=0:
+    for i in result:
+      print(i)
